6ecd5be6970b67d456d38e36e0c47f9200d17b68
Sean Walsh <seanmatthewwalsh@gmail.com>
2023-03-22 12:49:10 +0200

    download model

--- WEB/Controllers/DownloadsController.cs
+++ WEB/Controllers/DownloadsController.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.StaticFiles;
-using System.Net.Mime;
-using WEB.Controllers;
 using WEB.Models;
 using WEB.Reports.PDF;
 
-namespace KPI.Controllers
+namespace WEB.Controllers
 {
     [Route("api/[Controller]"), Authorize]
     public class DownloadsController : BaseApiController
@@ -21,37 +18,7 @@ namespace KPI.Controllers
 
             byte[] bytes = await report.GenerateAsync();
 
-            return DownloadResult(report.GetReportName(), bytes);
-        }
-
-        private FileContentResult DownloadResult(string fileName, byte[] fileContents)
-        {
-            Response.Headers.Add("Content-Disposition", GetContentDisposition(fileName).ToString());
-
-            return new FileContentResult(fileContents, GetContentType(fileName))
-            {
-                FileDownloadName = fileName
-            };
-        }
-
-        private string GetContentType(string fileName)
-        {
-            var provider = new FileExtensionContentTypeProvider();
-            string contentType;
-            if (!provider.TryGetContentType(fileName, out contentType))
-            {
-                contentType = "application/octet-stream";
-            }
-            return contentType;
-        }
-
-        private ContentDisposition GetContentDisposition(string fileName)
-        {
-            return new ContentDisposition
-            {
-                FileName = string.Format(fileName),
-                Inline = false,
-            };
+            return Download.GetFileContentResult(Response, report.GetReportName(), bytes);
         }
     }
 }
