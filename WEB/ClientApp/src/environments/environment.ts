486b1c430cc42716c675db1b0ea24469ba3fb3ef
Sean Walsh <seanmatthewwalsh@gmail.com>
2023-07-25 15:23:06 +0200

    set ports

---
+++ WEB/ClientApp/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    baseUrl: `${window.location.origin}/`,
+    // matches with launchSettings.json (which configures on which port the development api runs)
+    baseApiUrl: `https://localhost:44501/api/`,
+    baseAuthUrl: `https://localhost:44501/`,
+    siteName: 'WEBSITE 3'
+};
--- WEB/ClientApp/src/environments/sample.environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    baseApiUrl: `${window.location.origin}/api/`,
-    baseAuthUrl: `${window.location.origin}/`,
-    siteName: 'WEBSITE 3'
-};
